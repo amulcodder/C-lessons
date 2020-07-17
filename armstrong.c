0faed2adf1eb0d1cc4b49ad9d393d2dc7247e07f
amulcodder <68187229+amulcodder@users.noreply.github.com>
2020-07-17 13:38:21 +0530

    Armstrong number code

---
+++ armstrong.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void main()
+
+{
+
+   int n, sum = 0, t, remainder;
+
+   printf("\nPlease enter a number to find whether it is an armstrong or not:");
+
+   scanf("%d",&n);
+   t = n;
+
+   while( t != 0 )
+
+   {
+
+      remainder = t%10;
+
+      sum = sum + remainder*remainder*remainder;
+
+      t = t/10;
+
+   }
+
+   if ( n == sum )
+
+      printf("\nThe number %d is an armstrong number", n);
+
+   else
+
+      printf("\nThe number %d is not an armstrong number", n);
+
+   return 0;
+
+}
